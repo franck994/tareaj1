216718843c63926dceeaf5e63f10a44d0d67d3fc
franck994 <franck.cc789@gmail.com>
2022-05-13 04:06:15 +0000

    Examen De Unidad

--- ExamenDeUnidad/Examen.py
+++ ExamenDeUnidad/Examen.py
@@ -1,4 +1,14 @@
-def ejercicio1 ():
+def ejercicio1_FLIH ():
+    #1.- Diseñe un algoritmo que permita determinar el nivel del perfil de ingreso de un postulante con la 
+    #que obtiene una vacante a la carrera de ingeniería de Sistemas; considerando que, si su nota es mayor 
+    #o igual a 17 su nivel es 4, si la nota es menor de 17 y mayor o igual a 14 su nivel es 3, si su nota es 
+    #menor a 14 y mayor o igual a 11 su nivel es 2; mientras que si su nota es menor de 11 ya no puede 
+    #obtener la vacante y por ende también está en el nivel 1.
+    #Considera que la nota final es vigesimal según el siguiente ponderado:
+    #Examen de conocimiento 40%
+    #Entrevista Personal 35%
+    #Test Psicológico 25%
+
     #definir variables
     excom:float()
     enpe:float()
@@ -31,7 +41,13 @@ def ejercicio1 ():
     #datos de salida
     print(f"{nivel}")
     de
-def ejercico2 ():
+def ejercico2_FLIH ():
+    #2.-Tiendas Plaza Norte desea un programa que permita calcular el IVG de la venta de sus artefactos 
+    #además desea hacer un descuento de 10% cuando el monto base de venta supera los 2000 soles, 
+    #mientras que si el monto supera los 1000 desea hacer un descuento del 5% Y si supera los 500 soles 
+    #realiza un descuento del 2%, El algoritmo debe permitir calcular el IGV, el descuento y el monto total 
+    #a pagar.
+
     #definir variable
     igv:float()
     descu:float()
@@ -56,7 +72,13 @@ def ejercico2 ():
     print(f"el igv total es:{igv}")
     print(f"el descuento es:{descu}")
     print(f"el monto total a pagar es:{montotal}")
-def ejercicio3 ():
+def ejercicio3_FLIH ():
+    #3.-Diseñar un algoritmo que permita calcular una operación aritmética entre 2 valores introducidos y 
+    #el signo correspondiente por teclado: si es el signo es + debe realizar una suma, si es el signo – debe 
+    #realizar la resta, si es el signo / debe realizar la división, si es el signo * debe realizar la multiplicación, 
+    #si es el signo ^ debe calcular la potencia; por otro lado, si introduce R debe permitir calcular la raíz 
+    #cuadrada de un número, si introduce % debe permitir calcular el módulo de 2.
+
     #definir variable
     priN:float()
     seguN:float()
@@ -90,7 +112,12 @@ def ejercicio3 ():
         total=9
     #datos de salida
     print(f"el resultado es:{total}")
-def ejercicio4 ():
+def ejercicio4_FLIH ():
+    #4.-Secretaría de salud requiere un algoritmo que permita determinar qué tipo de vacuna (A, B o C) 
+    #aplicar a una persona, considerando que, si es mayor de 70, sin importar el sexo, se le aplica el tipo C; 
+    #si tiene entre 16 y 69 años, y es mujer se le aplica el tipo B, y si es hombre, la A; sí es menor de 16 
+    #años, se aplica el tipo A sin importar el sexo.
+
     #definir variable
     genero:str()
     edad:int()
@@ -109,7 +136,11 @@ def ejercicio4 ():
         mensaje=("Su vacuna es de tipo A")
     #Datos de salida
     print(mensaje)
-def ejercicio5():
+def ejercicio5_FLIH():
+    #5.-Realice un algoritmo que permita probar uno de los 3 algoritmos que ha diseñado anteriormente 
+    #aplicando una estructura condicional múltiple o algo similar, y las estructuras de control que se 
+    #requieran para tal fin.
+
     #Definir variables
     nivel=float()
     #Datos de entrada
@@ -118,16 +149,16 @@ def ejercicio5():
     if nivel>=5:
         print("Ese ejercicio no existe ingrese otro")
     elif nivel==4:
-        ejercicio4()
+        ejercicio4_FLIH()
     elif nivel==3:
-        ejercicio3()
+        ejercicio3_FLIH()
     elif nivel==2:
-        ejercicio2()
+        ejercicio2_FLIH()
     else:
-        ejercicio1()
+        ejercicio1_FLIH()
 
 
-ejercicio5()
+ejercicio5_FLIH()
     
 
   
