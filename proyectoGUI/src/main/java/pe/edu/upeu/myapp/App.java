297919f94cbd0478a41b6a7301582e4943cfb090
franck994 <franck.cc789@gmail.com>
2022-06-09 18:12:56 +0000

    proproyecto

---
+++ proyectoGUI/src/main/java/pe/edu/upeu/myapp/App.java
@@ -0,0 +1,18 @@
+package pe.edu
+
+import pe.edu.upeu.gui.MainGUI;
+
+,upeu.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new MainGUI();
+    }
+}
